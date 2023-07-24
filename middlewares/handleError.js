04444ef5d6e16f9e2a7d28f8e00625a46b97fb38
Pavel Shershnev <pavelsersnev@gmail.com>
2023-07-24 14:05:57 +1200

    test14-2

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -2,22 +2,22 @@ const mongoose = require('mongoose');
 
 module.exports = (err, req, res, next) => {
   if (err instanceof mongoose.Error.DocumentNotFoundError) {
-    return res.status(404).send({ messaege: 'Пользователь не найден' });
+    return res.status(404).send({ message: 'Пользователь не найден' });
   }
   if (err instanceof mongoose.Error.ValidationError || err instanceof mongoose.Error.CastError) {
-    return res.status(400).send({ messaege: 'Некорректные данные' });
+    return res.status(400).send({ message: 'Некорректные данные' });
   }
   if (err.code === 11000) {
-    return res.status(409).send({ messaege: 'Такой email уже используется' });
+    return res.status(409).send({ message: 'Такой email уже используется' });
   }
   if (err.message === 'Вы можете удалять только свои карточки') {
-    return res.status(403).send({ messaege: 'Вы можете удалять только свои карточки' });
+    return res.status(403).send({ message: 'Вы можете удалять только свои карточки' });
   }
   if (err.message === 'Необходима авторизация') {
-    return res.status(401).send({ messaege: 'Необходима авторизация' });
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
   if (err.message === 'Неправильная почта или пароль') {
-    return res.status(401).send({ messaege: 'Неправильная почта или пароль' });
+    return res.status(401).send({ message: 'Неправильная почта или пароль' });
   }
   return next(res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
