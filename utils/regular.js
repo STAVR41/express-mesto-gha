4574dcd9d14e5a6034bd5377dfa07ed5ded9263a
STAVR41 <97771486+STAVR41@users.noreply.github.com>
2023-07-26 08:23:21 +0300

    Update regular.js

--- utils/regular.js
+++ utils/regular.js
@@ -1,1 +1,1 @@
-module.exports.regular = /^(http|https):\/\/(www\.)?[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]{2,256}\.[a-zA-Z0-9./?#-]{2,}$/;
+module.exports.regular = /^(http|https):\/\/(www\.)?[a-zA-Z0-9\-._~:\/?#[\]@!$&'()*+,;=]{2,256}\.[a-zA-Z0-9.\/?#-]{2,}$/;
